ad99b8ff8bc48cec376f3182d409491d92983977
Bennet Yee <bennet.yee@gmail.com>
2024-05-30 11:27:05 -0700

    Some doc/comments

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # osu_hamming
 
 This repo contains experiments in writing a space-efficient (code/data) hamming code decoder, inspired by an honors thesis lightning talk.
+
+Note that the decoder is limited to 32768 symbols.
--- osu_hamming.h
+++ osu_hamming.h
@@ -9,9 +9,18 @@ struct HammingCodeNode {
   uint16_t children[2];
   /*
    * Index 0 is the left subtree and index 1 is the right subtree; a
-   * value of 0 means that the current node is a leaf node, and right
-   * contains symbol index instead.  There is never a node with only a
-   * left (or right subtree).
+   * value of 0 at index 0 means that the current node is a leaf node,
+   * and value at index 1 contains symbol index instead.  There is
+   * never a node with only a left (or right subtree).  Each tree node
+   * takes 4 bytes to encode.
+   *
+   * By using uint16_t we use 2 bytes for indices instead of 4 or 8
+   * for pointers (on 32- or 64-bit machines), and we (in effect)
+   * avoid having to cast the right pointer or use a void *.  This
+   * limits us to 2^{15} symbols, since a hamming code tree
+   * constructed for that would have 2^{16}-1 nodes.  If we need to be
+   * able to handle 2^{31} symbols, we would need to use uint32_t
+   * instead, and each node would take 8 bytes to encode.
    */
 };
 
